5a7fcb1a400826178ce07b55a8520abd4f3ade16
Clément Prévost <prevostclement@gmail.com>
2022-06-12 16:18:29 +0200

    Add ppfs import and use a default of 4h for block times

--- src/lib/csv-block-samples.ts
+++ src/lib/csv-block-samples.ts
@@ -10,10 +10,13 @@ import { makeDataDirRecursive } from "./make-data-dir-recursive";
 
 const CSV_SEPARATOR = ",";
 
-export type SamplingPeriod = "15min"; // | "1hour" | "1day";
-export const allSamplingPeriods = ["15min"];
+export type SamplingPeriod = "15min" | "1hour" | "4hour" | "1day";
+export const allSamplingPeriods = ["15min", "1hour", "4hour", "1day"];
 export const samplingPeriodMs: { [period in SamplingPeriod]: number } = {
   "15min": 15 * 60 * 1000,
+  "1hour": 60 * 60 * 1000,
+  "4hour": 4 * 60 * 60 * 1000,
+  "1day": 24 * 60 * 60 * 1000,
 };
 
 interface BlockSampleData {
