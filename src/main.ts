f20fd1ccf29f89fe89081054a10ba1c6fea7419b
Clément Prévost <prevostclement@gmail.com>
2022-06-25 17:15:35 +0100

    Import native transfer from strategies

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,4 @@ export * from "./import/import-block-samples";
 export * from "./import/import-vault-transfers";
 export * from "./import/import-vault-strategies";
 export * from "./import/import-vault-ppfs";
+export * from "./import/import-strategies-native-transfer-from";
