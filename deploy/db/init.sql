f03c735d0d6caa6d335e6bb77a082f1ac0e0eddb
Clément Prévost <prevostclement@gmail.com>
2022-07-02 22:47:48 +0200

    Add grafana read only user

---
+++ deploy/db/init.sql
@@ -0,0 +1,16 @@
+-- this will revoke default database privileges (CREATE, CONNECT ...) from roles in 'PUBLIC' (all roles). 
+REVOKE ALL ON DATABASE beefy FROM public; 
+REVOKE ALL ON SCHEMA public FROM public;
+REVOKE ALL ON DATABASE postgres FROM public;
+
+
+-- drop owned by grafana_ro cascade;
+-- drop user grafana_ro;
+CREATE USER grafana_ro WITH PASSWORD 'grafana_ro'
+NOSUPERUSER NOINHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+ALTER USER grafana_ro SET search_path = public,beefy_raw;
+
+GRANT CONNECT ON DATABASE beefy TO grafana_ro;
+GRANT USAGE ON SCHEMA public TO grafana_ro;
+GRANT USAGE ON SCHEMA beefy_raw TO grafana_ro;
+GRANT SELECT ON ALL TABLES IN SCHEMA beefy_raw TO grafana_ro;
--- src/scripts/load-timescaledb.ts
+++ src/scripts/load-timescaledb.ts
@@ -6,13 +6,17 @@ import { transform } from "stream-transform";
 import { Chain } from "../types/chain";
 import { stringify } from "csv-stringify";
 import { db_query_one, getPgPool, strAddressToPgBytea } from "../utils/db";
-import { getLocalBeefyVaultList } from "../lib/fetch-if-not-found-locally";
+import {
+  BeefyVault,
+  getLocalBeefyVaultList,
+} from "../lib/fetch-if-not-found-locally";
 import { logger } from "../utils/logger";
 import {
   ERC20EventData,
   getErc20TransferEventsStream,
 } from "../lib/csv-transfer-events";
 import { StreamObjectFilterTransform } from "../utils/stream";
+import { sleep } from "../utils/async";
 
 async function main() {
   const argv = await yargs(process.argv.slice(2))
@@ -26,8 +30,6 @@ async function main() {
   const chains = chain === "all" ? allChainIds : [chain];
   const vaultId = argv.vaultId || null;
 
-  const pgPool = await getPgPool();
-
   for (const chain of chains) {
     const vaults = await getLocalBeefyVaultList(chain);
     for (const vault of vaults) {
@@ -35,100 +37,116 @@ async function main() {
         logger.verbose(`[LTSDB] Skipping ${chain}:${vault.id}`);
         continue;
       }
-      logger.verbose(`[LTSDB] Processing ${chain}:${vault.id}`);
-      const contractAddress = vault.token_address;
-
-      const fileReadStream = await getErc20TransferEventsStream(
-        chain,
-        contractAddress
-      );
-      if (!fileReadStream) {
-        logger.verbose(`[LTSDB] No data for ${chain}:${vault.id}`);
-        continue;
+      try {
+        await importVault(chain, vault);
+      } catch (err) {
+        logger.error(
+          `[LTSDB] Skipping vault ${chain}:${vault.id}. ${JSON.stringify(err)}`
+        );
       }
-      // now get the last imported data to filter on those
-      const lastImportedDate =
-        (
-          await db_query_one<{ last_imported: Date }>(
-            `SELECT max(datetime) as last_imported
+    }
+  }
+
+  logger.info("[LTSDB] Finished importing data. Sleeping 4h...");
+  await sleep(4 * 60 * 60 * 1000);
+}
+
+async function importVault(chain: Chain, vault: BeefyVault) {
+  const pgPool = await getPgPool();
+
+  logger.verbose(`[LTSDB] Processing ${chain}:${vault.id}`);
+  const contractAddress = vault.token_address;
+
+  const fileReadStream = await getErc20TransferEventsStream(
+    chain,
+    contractAddress
+  );
+  if (!fileReadStream) {
+    logger.verbose(`[LTSDB] No data for ${chain}:${vault.id}`);
+    return;
+  }
+  // now get the last imported data to filter on those
+  const lastImportedDate =
+    (
+      await db_query_one<{ last_imported: Date }>(
+        `SELECT max(datetime) as last_imported
         FROM beefy_raw.erc20_transfer_ts
         WHERE chain = %L
           AND contract_address = %L`,
-            [chain, strAddressToPgBytea(contractAddress)]
-          )
-        )?.last_imported || null;
+        [chain, strAddressToPgBytea(contractAddress)]
+      )
+    )?.last_imported || null;
 
-      if (lastImportedDate) {
-        logger.verbose(
-          `[LTSDB] Only importing ${chain}:${
-            vault.id
-          } events after ${lastImportedDate.toISOString()}`
-        );
-      } else {
-        logger.verbose(
-          `[LTSDB] No data for ${chain}:${vault.id}, importing all events`
-        );
+  if (lastImportedDate) {
+    logger.verbose(
+      `[LTSDB] Only importing ${chain}:${
+        vault.id
+      } events after ${lastImportedDate.toISOString()}`
+    );
+  } else {
+    logger.verbose(
+      `[LTSDB] No data for ${chain}:${vault.id}, importing all events`
+    );
+  }
+
+  const onlyLatestRowsFilter = new StreamObjectFilterTransform<ERC20EventData>(
+    (row) => {
+      if (!lastImportedDate) {
+        return true;
       }
+      return row.datetime > lastImportedDate;
+    }
+  );
 
-      const onlyLatestRowsFilter =
-        new StreamObjectFilterTransform<ERC20EventData>((row) => {
-          if (!lastImportedDate) {
-            return true;
-          }
-          return row.datetime > lastImportedDate;
-        });
-
-      await new Promise((resolve, reject) => {
-        pgPool.connect(function (err, client, poolCallback) {
-          function onOk(...args: any[]) {
-            poolCallback(...args);
-            resolve(args);
-          }
-          function onErr(error: Error) {
-            poolCallback(error);
-            reject(error);
-          }
-          if (err) {
-            return reject(err);
-          }
-          const stream = client.query(
-            copyFrom(
-              `COPY beefy_raw.erc20_transfer_ts (chain, contract_address, datetime, from_address, to_address, value) 
+  await new Promise((resolve, reject) => {
+    pgPool.connect(function (err, client, poolCallback) {
+      function onOk(...args: any[]) {
+        poolCallback(...args);
+        resolve(args);
+      }
+      function onErr(error: Error) {
+        poolCallback(error);
+        reject(error);
+      }
+      if (err) {
+        return reject(err);
+      }
+      const stream = client.query(
+        copyFrom(
+          `COPY beefy_raw.erc20_transfer_ts (chain, contract_address, datetime, from_address, to_address, value) 
               FROM STDIN
               WITH CSV DELIMITER ',';`
-            )
-          );
-          fileReadStream.on("error", onErr);
-          stream.on("error", onErr);
-          stream.on("finish", onOk);
-          // start the work
-          fileReadStream
-            .pipe(onlyLatestRowsFilter)
-            .pipe(
-              transform(function (data: ERC20EventData) {
-                return [
-                  // these should match the order of the copy cmd
-                  chain,
-                  strAddressToPgBytea(contractAddress),
-                  data.datetime.toISOString(),
-                  strAddressToPgBytea(data.from),
-                  strAddressToPgBytea(data.to),
-                  data.value,
-                ];
-              })
-            )
-            .pipe(
-              stringify({
-                header: false,
-              })
-            )
-            .pipe(stream);
-        });
-      });
+        )
+      );
+      fileReadStream.on("error", onErr);
+      stream.on("error", onErr);
+      stream.on("finish", onOk);
+      // start the work
+      fileReadStream
+        .pipe(onlyLatestRowsFilter)
+        .pipe(
+          transform(function (data: ERC20EventData) {
+            return [
+              // these should match the order of the copy cmd
+              chain,
+              strAddressToPgBytea(contractAddress),
+              data.datetime.toISOString(),
+              strAddressToPgBytea(data.from),
+              strAddressToPgBytea(data.to),
+              data.value,
+            ];
+          })
+        )
+        .pipe(
+          stringify({
+            header: false,
+          })
+        )
+        .pipe(stream);
+    });
+  });
 
-      logger.debug(`[LTSDB] Finished processing ${chain}:${vault.id}`);
-    }
-  }
+  logger.debug(`[LTSDB] Finished processing ${chain}:${vault.id}`);
 }
 
 runMain(main);
