abadfd7fd79ee131ef42ca0968f6281a333e5404
Clément Prévost <prevostclement@gmail.com>
2022-10-07 18:08:21 +0200

    Only 1 item at a time when batching is disabled to ensure delay between calls is respected

--- src/protocol/common/utils/batch-rpc-calls.ts
+++ src/protocol/common/utils/batch-rpc-calls.ts
@@ -12,6 +12,7 @@ import { ethers } from "ethers";
 import { callLockProtectedRpc } from "../../../utils/shared-resources/shared-rpc";
 import { ArchiveNodeNeededError, isErrorDueToMissingDataFromNode } from "../../../utils/rpc/archive-node-needed";
 import { bufferUntilAccumulatedCountReached } from "../../../utils/rxjs/utils/buffer-until-accumulated-count";
+import { MIN_DELAY_BETWEEN_EXPLORER_CALLS_MS, MIN_DELAY_BETWEEN_RPC_CALLS_MS } from "../../../utils/config";
 
 const logger = rootLogger.child({ module: "utils", component: "batch-query-group" });
 
@@ -85,7 +86,11 @@ export function batchRpcCalls$<
   }
 
   if (!canUseBatchProvider) {
-    maxInputObjsPerBatch = options.streamConfig.maxInputTake;
+    if (MIN_DELAY_BETWEEN_RPC_CALLS_MS[options.rpcConfig.chain] === "no-limit") {
+      maxInputObjsPerBatch = options.streamConfig.maxInputTake;
+    } else {
+      maxInputObjsPerBatch = 1;
+    }
   }
   logger.trace({
     msg: "batchRpcCalls$ config. " + options.logInfos.msg,
