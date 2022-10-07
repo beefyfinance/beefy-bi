e5b664a5e914cfbf570db3895964dca716c3c9cb
Clément Prévost <prevostclement@gmail.com>
2022-10-07 14:47:12 +0200

    Add rpc url to rpc traces

--- src/utils/rpc/remove-secrets-from-rpc-url.ts
+++ src/utils/rpc/remove-secrets-from-rpc-url.ts
@@ -1,8 +1,8 @@
 export function removeSecretsFromRpcUrl(secretRpcUrl: string): string {
   const urlObj = new URL(secretRpcUrl);
-  let publicRpcUrl = urlObj.protocol + "//" + urlObj.hostname;
+  let publicRpcUrl = urlObj.protocol + "//" + urlObj.hostname + (urlObj.port.length ? ":" + urlObj.port : "");
   if (secretRpcUrl.includes("ankr")) {
-    publicRpcUrl += urlObj.pathname.split("/")[0];
+    publicRpcUrl += "/" + urlObj.pathname.split("/")[1];
   }
   return publicRpcUrl;
 }
