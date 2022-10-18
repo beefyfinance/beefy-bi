47d18c3fa45e58bbf17c8542be0687f9392d4364
Clément Prévost <prevostclement@gmail.com>
2022-10-18 21:18:44 +0200

    Fix sampling

--- src/types/sampling.ts
+++ src/types/sampling.ts
@@ -1,8 +1,8 @@
-export type SamplingPeriod = "15s" | "30s" | "1min" | "5min" | "15min" | "1hour" | "4hour" | "1day";
-export const allSamplingPeriods: SamplingPeriod[] = ["15s", "30s", "1min", "5min", "15min", "1hour", "4hour", "1day"];
+export type SamplingPeriod = "15sec" | "30sec" | "1min" | "5min" | "15min" | "1hour" | "4hour" | "1day";
+export const allSamplingPeriods: SamplingPeriod[] = ["15sec", "30sec", "1min", "5min", "15min", "1hour", "4hour", "1day"];
 export const samplingPeriodMs: { [period in SamplingPeriod]: number } = {
-  "15s": 15 * 1000,
-  "30s": 30 * 1000,
+  "15sec": 15 * 1000,
+  "30sec": 30 * 1000,
   "1min": 1 * 60 * 1000,
   "5min": 5 * 60 * 1000,
   "15min": 15 * 60 * 1000,
