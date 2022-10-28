15c7d3f526c181ad60a19a3800567f3206ccdb3c
Clément Prévost <prevostclement@gmail.com>
2022-10-28 11:10:20 +0200

    Add investor timeline

--- src/api/route/portfolio.ts
+++ src/api/route/portfolio.ts
@@ -7,26 +7,30 @@ export default async function (instance: FastifyInstance, opts: FastifyPluginOpt
   const schema = {
     querystring: S.object().prop("address", addressSchema),
   };
+
   instance.get<{
     Querystring: { address: string };
-  }>(
-    "/",
-    {
-      schema,
-      config: {
-        rateLimit: await getRateLimitOpts(),
-      },
-    },
-    async (req, reply) => {
-      const { address } = req.query;
-      const investorId = await instance.diContainer.cradle.investor.getInvestorId(address);
-      if (investorId === null) {
-        return reply.code(404).send({ error: "Investor not found" });
-      }
-      const productValues = await instance.diContainer.cradle.portfolio.getInvestorPortfolioValue(investorId);
-      return reply.send(productValues);
-    },
-  );
+  }>("/", { schema, config: { rateLimit: await getRateLimitOpts() } }, async (req, reply) => {
+    const { address } = req.query;
+    const investorId = await instance.diContainer.cradle.investor.getInvestorId(address);
+    if (investorId === null) {
+      return reply.code(404).send({ error: "Investor not found" });
+    }
+    const productValues = await instance.diContainer.cradle.portfolio.getInvestorPortfolioValue(investorId);
+    return reply.send(productValues);
+  });
+
+  instance.get<{
+    Querystring: { address: string };
+  }>("/timeline", { schema, config: { rateLimit: await getRateLimitOpts() } }, async (req, reply) => {
+    const { address } = req.query;
+    const investorId = await instance.diContainer.cradle.investor.getInvestorId(address);
+    if (investorId === null) {
+      return reply.code(404).send({ error: "Investor not found" });
+    }
+    const productValues = await instance.diContainer.cradle.portfolio.getInvestorTimeline(investorId);
+    return reply.send(productValues);
+  });
 
   done();
 }
--- src/api/service/portfolio.ts
+++ src/api/service/portfolio.ts
@@ -1,6 +1,6 @@
 import Decimal from "decimal.js";
 import { PoolClient } from "pg";
-import { string } from "yargs";
+import { boolean, string } from "yargs";
 import { allChainIds, Chain } from "../../types/chain";
 import { db_query } from "../../utils/db";
 import { AsyncCache } from "./cache";
@@ -33,7 +33,7 @@ export class PortfolioService {
   }
 
   async getInvestorPortfolioValue(investorId: number) {
-    const cacheKey = `api:portfolio-service:${investorId}}`;
+    const cacheKey = `api:portfolio-service:current-value:${investorId}}`;
     const ttl = 1000 * 60 * 5; // 5 min
     return this.services.cache.wrap(cacheKey, ttl, async () => {
       const investedProducts = await this.getInvestedProducts(investorId, allChainIds);
@@ -95,9 +95,9 @@ export class PortfolioService {
             coalesce(p.product_data->'vault'->>'eol', p.product_data->'boost'->>'eol')::text = 'true' as is_eol,
             p1.price as share_to_underlying_price, 
             p2.price as underlying_to_usd_price,
-            b.share_balance::evm_decimal_256, 
-            (b.share_balance * p1.price)::evm_decimal_256 as underlying_balance,
-            (b.share_balance * p1.price * p2.price)::evm_decimal_256 as usd_balance
+            b.share_balance::NUMERIC(100, 24), 
+            (b.share_balance * p1.price)::NUMERIC(100, 24) as underlying_balance,
+            (b.share_balance * p1.price * p2.price)::NUMERIC(100, 24) as usd_balance
           from share_balance b
             left join product p on b.product_id = p.product_id
             left join price_1 p1 on p.price_feed_1_id = p1.price_feed_id
@@ -105,6 +105,72 @@ export class PortfolioService {
           order by 1
         `,
         [investorId, productIds, priceFeed1Ids, priceFeed2Ids],
+        this.services.db,
+      );
+    });
+  }
+
+  async getInvestorTimeline(investorId: number) {
+    const cacheKey = `api:portfolio-service:timeline:${investorId}}`;
+    const ttl = 1000 * 60 * 5; // 5 min
+    return this.services.cache.wrap(cacheKey, ttl, async () => {
+      const investedProducts = await this.getInvestedProducts(investorId, allChainIds);
+      const productIds = investedProducts.map((p) => p.product_id);
+
+      return db_query<{
+        datetime: Date;
+        product_key: string;
+        chain: Chain;
+        is_eol: boolean;
+        share_to_underlying_price: string;
+        underlying_to_usd_price: string;
+        share_balance: string;
+        underlying_balance: string;
+        usd_balance: string;
+        share_diff: string;
+        underlying_diff: string;
+        usd_diff: string;
+      }>(
+        `
+          with investment_diff_raw as (
+            select b.datetime, b.product_id, b.balance, b.balance_diff, last(pr1.price::numeric, pr1.datetime) as price1, last(pr2.price::numeric, pr2.datetime) as price2
+            from investment_balance_ts b
+            left join product p 
+              on b.product_id = p.product_id
+            -- we should have the exact price1 (share to underlying) from this exact block for all investment change
+            left join price_ts pr1 
+              on p.price_feed_1_id = pr1.price_feed_id 
+              and pr1.datetime = b.datetime 
+              and pr1.block_number = b.block_number 
+            -- but for price 2 (underlying to usd) we need to match on approx time
+            left join price_ts pr2 
+              on p.price_feed_2_id = pr2.price_feed_id 
+              and time_bucket('15min', pr2.datetime) = time_bucket('15min', b.datetime)
+            where b.investor_id = %L
+              and b.product_id in (select unnest(ARRAY[%L]::integer[]))
+            group by 1,2,3,4
+          ),
+          investment_diff as (
+            select b.datetime,
+              p.product_key,
+              p.chain,
+              coalesce(p.product_data->'vault'->>'eol', p.product_data->'boost'->>'eol')::text as is_eol,
+              b.price1 as share_to_underlying_price, 
+              b.price2 as underlying_to_usd_price,
+              b.balance as share_balance, 
+              (b.balance * b.price1)::NUMERIC(100, 24) as underlying_balance,
+              (b.balance * b.price1 * b.price2)::NUMERIC(100, 24) as usd_balance,
+              b.balance_diff as share_diff, 
+              (b.balance_diff * b.price1)::NUMERIC(100, 24) as underlying_diff,
+              (b.balance_diff * b.price1 * b.price2)::NUMERIC(100, 24) as usd_diff
+            from investment_diff_raw b
+            join product p on p.product_id = b.product_id
+          )
+          select * 
+          from investment_diff
+          order by product_key asc, datetime asc
+        `,
+        [investorId, productIds],
         this.services.db,
       );
     });
