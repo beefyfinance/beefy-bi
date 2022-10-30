94d7a5c8e082ed43cfa43dd65c02753e39780643
Clément Prévost <prevostclement@gmail.com>
2022-10-30 18:03:26 +0100

    Fix default config dir

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -232,7 +232,7 @@ export const MIN_DELAY_BETWEEN_RPC_CALLS_MS: {
   syscoin: _getDelayFromEnv("syscoin"),
 };
 
-export const CONFIG_DIRECTORY = process.env.DATA_DIRECTORY || path.join(__dirname, "..", "..", "data", "config");
+export const CONFIG_DIRECTORY = process.env.CONFIG_DIRECTORY || path.join(__dirname, "..", "..", "data", "config");
 
 export const DATA_DIRECTORY = process.env.DATA_DIRECTORY || path.join(__dirname, "..", "..", "data", "indexed-data");
 
