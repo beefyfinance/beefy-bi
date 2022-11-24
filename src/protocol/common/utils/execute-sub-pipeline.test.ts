f7620e93c663c5306f150aea19ac0b4d3194c6dc
Clément Prévost <prevostclement@gmail.com>
2022-11-24 15:10:07 +0100

    Make sure we mark item as in error when the subpipeline returns the item multiple times

--- src/protocol/common/utils/execute-sub-pipeline.test.ts
+++ src/protocol/common/utils/execute-sub-pipeline.test.ts
@@ -138,4 +138,50 @@ describe("executeSubPipeline$", () => {
     ]);
     expect(errors).toEqual([{ id: 2, targets: [4, 5, 6] }]);
   });
+
+  it("should detect when sub-pipeline returns a sub-item multiple times", async () => {
+    const input$ = Rx.from([
+      { id: 1, targets: [1, 2, 3] },
+      { id: 2, targets: [4, 5, 6] },
+      { id: 3, targets: [7, 8, 9] },
+    ]);
+
+    const errors: any[] = [];
+    const ctx: any = { streamConfig: { maxBatchSize: 100, maxInputWaitMs: 100 } };
+
+    const pipeline$ = input$.pipe(
+      executeSubPipeline$({
+        ctx,
+        emitError: (obj) => errors.push(obj),
+        getObjs: (obj) => obj.targets,
+        pipeline: (emitError) =>
+          Rx.pipe(
+            Rx.connect((items$) =>
+              Rx.merge(
+                items$.pipe(
+                  Rx.filter((item) => item.parent.id !== 2),
+                  // do nothing
+                ),
+                items$.pipe(
+                  Rx.filter((item) => item.parent.id === 2),
+                  // duplicate sub-items
+                  Rx.concatMap((item) => [item, item, item]),
+                ),
+              ),
+            ),
+            Rx.map((item) => ({ ...item, result: item.target * 2 })),
+          ),
+        formatOutput: (obj, result) => ({ ...obj, result: obj.targets.map((t) => result.get(t)) }),
+      }),
+      Rx.toArray(),
+    );
+
+    const result = await consumeObservable(pipeline$);
+    console.dir({ result, errors });
+    expect(result).toEqual([
+      { id: 1, targets: [1, 2, 3], result: [2, 4, 6] },
+      { id: 3, targets: [7, 8, 9], result: [14, 16, 18] },
+    ]);
+    expect(errors).toEqual([{ id: 2, targets: [4, 5, 6] }]);
+  });
 });
