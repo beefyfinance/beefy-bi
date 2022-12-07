38084b61913a0302f8d7e2ed49c745d0293c30cb
Clément Prévost <prevostclement@gmail.com>
2022-12-07 13:42:39 +0100

    Cleanup test files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
 export * from "./api/api";
-export * from "./protocol/beefy/script/fix-ppfs-anomalies";
 export * from "./script/find-out-rpc-limitations";
-export * from "./script/poc_harvests";
 export * from "./script/run";
-export * from "./script/test";
