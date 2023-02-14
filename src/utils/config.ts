3e511a271919f0f41317f7fb694d61befb6e8ebd
chebiN <17909998+seguido@users.noreply.github.com>
2023-02-14 14:08:04 -0300

    parse app pr builds as regex

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -24,7 +24,7 @@ export const API_LISTEN = process.env.API_LISTEN || "127.0.0.1";
 export const API_DISABLE_HTTPS = process.env.API_DISABLE_HTTPS === "true";
 export const API_URL = process.env.API_URL || "http://localhost:8080";
 export const API_FRONTEND_URL = process.env.API_FRONTEND_URL || "http://localhost:3001";
-export const APP_PR_BUILDS_URL = process.env.APP_PR_BUILDS_URL || "http://localhost:3000";
+export const APP_PR_BUILDS_URL = process.env.APP_PR_BUILDS_URL ? new RegExp(process.env.APP_PR_BUILDS_URL) : "http://localhost:3000";
 export const APP_LOCAL_BUILDS_URL = process.env.APP_LOCAL_BUILDS_URL || "http://localhost:3001";
 
 export const TIMESCALEDB_URL = process.env.TIMESCALEDB_URL || "psql://beefy:beefy@localhost:5432/beefy";
